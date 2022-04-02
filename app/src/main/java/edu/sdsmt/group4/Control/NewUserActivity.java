6c0459f5f79cc2e2bce1b1cdb16638c64cdb83c7
7555105 <7555105@T-SMD1031827>
2022-04-02 16:17:21 -0600

    Added new user activity

---
+++ app/src/main/java/edu/sdsmt/group4/Control/NewUserActivity.java
@@ -0,0 +1,15 @@
+package edu.sdsmt.group4.Control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import edu.sdsmt.group4.R;
+
+public class NewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+    }
+}
